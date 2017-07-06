d5af5ec82a09567754947e20bf870b36b801fb50
stack07142 <stack.07142@gmail.com>
2017-07-06 20:36:55 +0900

    gradle 설정

---
+++ app/src/main/java/io/github/stack07142/trendingingithub/RepoListActivity.java
@@ -0,0 +1,15 @@
+package io.github.stack07142.trendingingithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo_list);
+
+
+    }
+}
